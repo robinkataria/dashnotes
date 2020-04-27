08e78bc4d069dd9c2e4a2170365d19b6208c047c
Robin Kataria <robin8kataria@gmail.com>
2020-04-28 02:01:32 +0530

    mongodb setup

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -15,18 +14,18 @@ const aboutContent =
     "Ipsum augue eget arcu dictum varius duis at consectetur lorem. Justo donec enim diam vulputate ut. At auctor urna nunc id cursus metus. Viverra orci sagittis eu volutpat odio facilisis mauris sit. In ornare quam viverra orci. Purus sit amet volutpat consequat. Diam quis enim lobortis scelerisque fermentum dui faucibus in. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Quis ipsum suspendisse ultrices gravida dictum. Aenean et tortor at risus viverra adipiscing at in tellus. Sed risus ultricies tristique nulla aliquet enim tortor at auctor. Mi bibendum neque egestas congue quisque egestas diam. Ullamcorper morbi tincidunt ornare massa. Libero nunc consequat interdum varius sit amet mattis vulputate. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Duis at consectetur lorem donec massa sapien faucibus et molestie. Tristique senectus et netus et malesuada.";
 const contactContent =
     "Fusce ut placerat orci nulla pellentesque dignissim enim. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Eget duis at tellus at. Urna neque viverra justo nec ultrices dui sapien eget mi. Luctus accumsan tortor posuere ac ut consequat semper. Vitae turpis massa sed elementum tempus egestas. Amet risus nullam eget felis eget. Luctus accumsan tortor posuere ac ut consequat semper viverra. Eget nulla facilisi etiam dignissim diam. Justo eget magna fermentum iaculis eu. Hac habitasse platea dictumst quisque sagittis purus sit amet. Augue ut lectus arcu bibendum at varius vel pharetra vel. Integer enim neque volutpat ac tincidunt vitae. Ut pharetra sit amet aliquam id diam. Enim nec dui nunc mattis enim ut.";
-const posts = [];
+let posts = [];
 
 // --------- mongoDB ----------
 
-mongoose.connect("mongodb://localhost:27017/postsDB", {
+mongoose.connect("mongodb://localhost:27017/blogDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 const postSchema = new mongoose.Schema({
     title: String,
-    post: String,
+    content: String,
 });
 
 const Post = mongoose.model("Post", postSchema);
@@ -34,7 +33,12 @@ const Post = mongoose.model("Post", postSchema);
 // --------- Routes -----------
 
 app.get("/", (req, res) => {
-    res.render("home", { startingContent: homeStartingContent, posts: posts });
+    Post.find((err, posts) => {
+        if (err) return handleError(err);
+        else {
+            res.render("home", { startingContent: homeStartingContent, posts: posts });
+        }
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -49,19 +53,25 @@ app.get("/compose", (req, res) => {
     res.render("compose");
 });
 
-app.get("/posts/:parameter", (req, res) => {
-    posts.forEach((post) => {
-        if (_.lowerCase(req.params.parameter) === _.lowerCase(post.title)) {
-            res.render("post", { content: post });
-        }
+app.get("/posts/:postid", (req, res) => {
+    Post.findOne({ _id: req.params.postid }, (err, post) => {
+        res.render("post", { content: post });
     });
 });
 
 app.post("/compose", (req, res) => {
-    const postData = { title: req.body.postTitle, content: req.body.postBody };
-    posts.push(postData);
+    const newPost = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody,
+    });
 
-    res.redirect("/");
+    // newPost.save();
+    Post.create(newPost, (err, newPost) => {
+        if (err) return handleError(err);
+        else {
+            res.redirect("/");
+        }
+    });
 });
 
 // ---------Listen-------------
